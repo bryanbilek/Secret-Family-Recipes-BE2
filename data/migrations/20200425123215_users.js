1d7e5ffc9299f7df831a520f865f13236e5d5518
bryanbilek <bbilek35@gmail.com>
2020-04-25 13:07:06 -0500

    started creating tbls in migrations

---
+++ data/migrations/20200425123215_users.js
@@ -0,0 +1,48 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        //each field required; username has to be unique
+        .createTable('users', tbl => {
+            tbl.increments();
+            tbl.string('first_name').notNullable();
+            tbl.string('last_name').notNullable();
+            tbl.string('email').notNullable();
+            tbl.string('username', 12).notNullable().unique();//max 12 characters
+            tbl.string('password', 12).notNullable();//max 12 characters
+        })
+
+        //each field except image required; user_id refers to users tbl
+        .createTable('recipes', tbl => {
+            tbl.increments();
+            tbl.integer('user_id').references('id').inTable('users').unsigned().notNullable().onUpdate('CASCADE').onDelete('CASCADE');
+            tbl.varBinary('image');//need to figure out the image data type
+            tbl.string('recipe_name').notNullable();
+            tbl.text('description', 255).notNullable();//keeping description to a max of 255 chars
+            tbl.string('prep_time').notNullable();
+            tbl.string('cook_time').notNullable();
+        })
+
+        //each field required; recipe_id refers to recipes tbl
+        .createTable('ingredients', tbl => {
+            tbl.increments();
+            tbl.integer('recipe_id').references('id').inTable('recipes').unsigned().notNullable().onUpdate('CASCADE').onDelete('CASCADE');
+            tbl.string('ingredient_name').notNullable();
+            tbl.integer('ingredient_amount').notNullable();
+        })
+
+        //each field required; recipe_id refers to recipes tbl
+        .createTable('steps', tbl => {
+            tbl.increments();
+            tbl.integer('recipe_id').references('id').inTable('recipes').unsigned().notNullable().onUpdate('CASCADE').onDelete('CASCADE');
+            tbl.integer('step_number').notNullable().unsigned();
+            tbl.text('step_instruction', 255).notNullable();//keeping the instructions to a max of 255 chars
+        });
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('steps')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes')
+        .dropTableIfExists('users');
+};
